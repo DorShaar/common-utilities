6640410905228851e3852e5cde9fb5f84a746830
Dor Shaar <dorsh@cymulate.com>
2023-04-11 18:08:36 +0300

    Add TempDirectory and TempFile

---
+++ Temporaries/TempDirectory.cs
@@ -0,0 +1,40 @@
+﻿namespace Temporaries;
+
+public class TempDirectory : IDisposable
+{
+    #region Properties
+    public string Path { get; }
+    #endregion Properties
+
+    public TempDirectory()
+    {
+        Path = Directory.CreateDirectory(System.IO.Path.GetRandomFileName()).FullName;
+    }
+
+    public TempDirectory(string directoryPath)
+    {
+        Path = Directory.CreateDirectory(directoryPath).FullName;
+    }
+
+    public static TempDirectory CreateTemporaryDirectory(string iDirName)
+    {
+        TempDirectory tempFile = new(iDirName);
+
+        return tempFile;
+    }
+
+    public static TempDirectory CreateTemporaryDirectory()
+    {
+        TempDirectory tempFile = new();
+
+        return tempFile;
+    }
+
+    public void Dispose()
+    {
+        if (Directory.Exists(Path))
+        {
+            Directory.Delete(Path, recursive: true);
+        }
+    }
+}
---
+++ Temporaries/TempFile.cs
@@ -0,0 +1,47 @@
+﻿namespace Temporaries;
+
+public class TempFile : IDisposable
+{
+    public string Path { get; }
+
+    /// <summary>
+    /// Creating a file with random name. 
+    /// </summary>
+    public TempFile()
+    {
+        Path = System.IO.Path.GetRandomFileName();
+    }
+
+    /// <summary>
+    /// Assuming file <see cref="iPath"/> is already created 
+    /// </summary>
+    public TempFile(string filePath)
+    {
+        Path = filePath;
+    }
+
+    public static TempFile CopyFromExistingFile(string existingFile)
+    {
+        if (!File.Exists(existingFile))
+        {
+            throw new FileNotFoundException(existingFile);
+        }
+
+        TempFile tempFile = new(System.IO.Path.GetRandomFileName());
+        File.Copy(existingFile, tempFile.Path);
+
+        return tempFile;
+    }
+
+    public static TempFile CreateZeroBytesTemporaryFile()
+    {
+        TempFile tempFile = new(System.IO.Path.GetTempFileName());
+
+        return tempFile;
+    }
+
+    public void Dispose()
+    {
+        File.Delete(Path);
+    }
+}
