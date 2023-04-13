f1fc80b4ba7da818e29c01ea10d60ace4e8d266f
Dor Shaar <dorsh@cymulate.com>
2023-04-14 02:03:06 +0300

    Remove cloner and PrototypeMachine since it uses BinaryFormatter which is obsolete, added Admin to OSOperations

---
+++ OSOperations/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace OSOperations;
+
+public class Admin
+{
+    public static bool IsRunningAsAdministrator()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+#pragma warning disable CA1416
+            WindowsIdentity windowsIdentity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal windowsPrincipal = new(windowsIdentity);
+            return windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator);
+#pragma warning restore CA1416                
+        }
+
+        return true;
+    }
+}
