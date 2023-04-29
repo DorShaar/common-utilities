97c96671f20c2a5c8a92ae20f43de732b14f71b2
Dor Shaar <dorsh@cymulate.com>
2023-04-29 12:07:49 +0300

    Add JsonSerializer

---
+++ JsonSerialization/IJsonSerializer.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonSerialization;
+
+public interface IJsonSerializer
+{
+    void Serialize<T>(T objectToSerialize, string outputPath);
+    T Deserialize<T>(string inputPath);
+    void AddConverters(JsonConverter jsonConverter);
+}
---
+++ JsonSerialization/JsonSerializer.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonSerialization;
+
+public class JsonSerializer : IJsonSerializer
+{
+    private readonly JsonSerializerSettings mSettings = new()
+    {
+        NullValueHandling = NullValueHandling.Ignore,
+    };
+    
+    public void Serialize<T>(T objectToSerialize, string databasePath)
+    {
+        string jsonText = JsonConvert.SerializeObject(objectToSerialize, Formatting.Indented);
+        File.WriteAllText(databasePath, jsonText);
+        Console.WriteLine($"Serialized object {typeof(T)} into {databasePath}");
+    }
+
+    public T Deserialize<T>(string databasePath)
+    {
+        T deserializedObject = JsonConvert.DeserializeObject<T>(File.ReadAllText(databasePath), mSettings)
+                               ?? throw new NullReferenceException($"Failed to deserialize '{databasePath}'");
+        Console.WriteLine($"Deserialized object {typeof(T)} from {databasePath}");
+            
+        return deserializedObject;
+    }
+
+    public void AddConverters(JsonConverter jsonConverter)
+    {
+        mSettings.Converters.Add(jsonConverter);
+    }
+}
