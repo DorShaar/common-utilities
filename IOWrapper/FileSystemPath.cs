6a6a2e49ceae5918b0618876d71308205624bced
Dor Shaar <dorsh@cymulate.com>
2023-05-06 21:20:08 +0300

    Added FileSystemPath

---
+++ IOWrapper/FileSystemPath.cs
@@ -0,0 +1,54 @@
+﻿namespace IOWrapper;
+
+public class FileSystemPath
+{
+    public string PathString { get; }
+    
+    public FileSystemPath(string path)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            throw new ArgumentException($"{nameof(path)} is empty");
+        }
+        
+        PathString = PreparePath(path);
+    }
+
+    public override string ToString()
+    {
+        return PathString;
+    }
+
+    public FileSystemPath Replace(string oldValue, string newValue)
+    {
+        FileSystemPath oldPath = new(oldValue); 
+        FileSystemPath newPath = new(newValue); 
+        string replacedPathString = PathString.Replace(oldPath.PathString, newPath.PathString);
+        return new FileSystemPath(replacedPathString);
+    }
+
+    // TODO DOR now tests
+    public FileSystemPath GetRelativePath(string relativeTo)
+    {
+        FileSystemPath relativeToPath = new(relativeTo);
+        if (!PathString.StartsWith(relativeToPath.PathString))
+        {
+            throw new ArgumentException($"Path {PathString} is not relative to {relativeTo}");
+        }
+
+        string relativePathString = PathString.Replace(relativeTo, string.Empty);
+        return new FileSystemPath(relativePathString);
+    }
+
+    private static string PreparePath(string path)
+    {
+        string pathWithUnixCharSeparators = path.Replace('\\', '/');
+        
+        if (!Path.IsPathRooted(pathWithUnixCharSeparators) && pathWithUnixCharSeparators[0] != '/')
+        {
+            pathWithUnixCharSeparators = '/' + pathWithUnixCharSeparators;
+        }
+
+        return pathWithUnixCharSeparators.TrimEnd('/');
+    }
+}
---
+++ Tests/IOWrapper/FileSystemPathTests.cs
@@ -0,0 +1,103 @@
+﻿using IOWrapper;
+using Xunit;
+
+namespace Tests.IOWrapper;
+
+public class FileSystemPathTests
+{
+    [Fact]
+    public void Ctor_RootedPath_WindowsCharSeparators_PathStringAsExpected()
+    {
+        FileSystemPath path = new("C:\\dor\\someDir");
+        Assert.Equal("C:/dor/someDir", path.PathString);
+    }
+    
+    [Fact]
+    public void Ctor_RootedPath_UnixCharSeparators_PathStringAsExpected()
+    {
+        FileSystemPath path = new("C:/dor/someDir");
+        Assert.Equal("C:/dor/someDir", path.PathString);
+    }
+    
+    [Fact]
+    public void Ctor_NonRootedPath_HasCharSeparatorAtTheBeginning_WindowsCharSeparators_PathStringAsExpected()
+    {
+        FileSystemPath path = new("\\dor\\someDir");
+        Assert.Equal("/dor/someDir", path.PathString);
+    }
+    
+    [Fact]
+    public void Ctor_NonRootedPath_HasCharSeparatorAtTheBeginning_UnixCharSeparators_PathStringAsExpected()
+    {
+        FileSystemPath path = new("/dor/someDir");
+        Assert.Equal("/dor/someDir", path.PathString);
+    }
+    
+    [Fact]
+    public void Ctor_NonRootedPath_HasNoCharSeparatorAtTheBeginning_WindowsCharSeparators_PathStringAsExpected()
+    {
+        FileSystemPath path = new("dor\\someDir");
+        Assert.Equal("/dor/someDir", path.PathString);
+    }
+    
+    [Fact]
+    public void Ctor_NonRootedPath_HasNoCharSeparatorAtTheBeginning_UnixCharSeparators_PathStringAsExpected()
+    {
+        FileSystemPath path = new("dor/someDir");
+        Assert.Equal("/dor/someDir", path.PathString);
+    }
+    
+    [Fact]
+    public void Ctor_HasCharSeparatorAtTheEnd_WindowsCharSeparators_PathStringAsExpected()
+    {
+        FileSystemPath path = new("dor\\someDir\\");
+        Assert.Equal("/dor/someDir", path.PathString);
+    }
+    
+    [Fact]
+    public void Ctor_HasCharSeparatorAtTheEnd_UnixCharSeparators_PathStringAsExpected()
+    {
+        FileSystemPath path = new("dor/someDir/");
+        Assert.Equal("/dor/someDir", path.PathString);
+    }
+
+    [Fact]
+    public void Replace_OnePathHasCharSeparatorAtTheBeginningAndTheOtherDont_ReplacedAsExpected()
+    {
+        FileSystemPath path = new("C:\\dor\\folder1\\games\\princeOfPersia");
+        const string oldValue = "games/princeOfPersia";
+        const string newValue = "/nonDosGames/commandos";
+
+        FileSystemPath replacedPath = path.Replace(oldValue, newValue);
+        Assert.Equal("C:/dor/folder1/nonDosGames/commandos", replacedPath.PathString);
+    }
+
+    [Fact]
+    public void GetRelativePath_PathAreNotRelative_ThrowsArgumentException()
+    {
+        FileSystemPath path = new("C:\\dor\\folder1\\games\\princeOfPersia");
+        const string nonRelativeToPath = "nonRelativePath";
+
+        Assert.Throws<ArgumentException>(() => path.GetRelativePath(nonRelativeToPath));
+    }
+    
+    [Fact]
+    public void GetRelativePath_RelativeToIsEmpty_ThrowsArgumentException()
+    {
+        FileSystemPath path = new("C:\\dor\\folder1\\games\\princeOfPersia");
+        const string nonRelativeToPath = "";
+
+        Assert.Throws<ArgumentException>(() => path.GetRelativePath(nonRelativeToPath));
+    }
+    
+    [Fact]
+    public void GetRelativePath_PathAreRelative_ReturnsRelativePath()
+    {
+        FileSystemPath path = new("C:\\dor\\folder1\\games\\princeOfPersia");
+        const string relativeTo = "C:/dor/folder1/";
+
+        FileSystemPath relativePath = path.GetRelativePath(relativeTo);
+        
+        Assert.Equal("/games/princeOfPersia", relativePath.PathString);
+    }
+}
