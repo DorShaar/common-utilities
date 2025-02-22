ed9e11248adec3e478d2b11c82bacd4e89c2b59c
Dor Shaar <dorsr@mta.ac.il>
2025-02-22 12:44:41 +0200

    Add bytes handler

---
+++ BytesHandler/BytesReader.cs
@@ -0,0 +1,29 @@
+﻿namespace BytesHandler
+{
+    public class BytesReader
+    {
+        /// <summary>
+        /// When buffer is in the next structre: <size_1><bytes_1><size_2><bytes_2>...<size_n><bytes_n>
+        /// </summary>
+        public static List<byte[]> ReadBytesFromBuffer(byte[] buffer)
+        {
+            List<byte[]> subBuffers = [];
+            int sourceOffset = 0;
+            while (sourceOffset < buffer.Length)
+            {
+                byte[] sizeInBytes = new byte[4];
+                Buffer.BlockCopy(buffer, sourceOffset, sizeInBytes, 0, sizeInBytes.Length);
+                sourceOffset += sizeInBytes.Length;
+
+                int bytesLength = BitConverter.ToInt32(sizeInBytes, 0);
+                byte[] subBuffer = new byte[bytesLength];
+                Buffer.BlockCopy(buffer, sourceOffset, subBuffer, 0, subBuffer.Length);
+                sourceOffset += subBuffer.Length;
+
+                subBuffers.Add(subBuffer);
+            }
+
+            return subBuffers;
+        }
+    }
+}
---
+++ BytesHandler/BytesWriter.cs
@@ -0,0 +1,40 @@
+﻿namespace BytesHandler
+{
+    public static class BytesWriter
+    {
+        /// <summary>
+        /// Writting to buffer several byte[] with suffix of their size in order to read them according to their size.
+        /// </summary>
+        public static async Task<byte[]> WriteFilesToBuffer(Dictionary<string, int> fileToSizeMap,
+            CancellationToken cancellationToken)
+        {
+            byte[] buffer = prepareBuffer(fileToSizeMap);
+            int bufferOffset = 0;
+
+            foreach ((string fileToUpload, int _) in fileToSizeMap)
+            {
+                byte[] fileBytes = await File.ReadAllBytesAsync(fileToUpload, cancellationToken).ConfigureAwait(false);
+                byte[] sizeInBytesRepresentation = BitConverter.GetBytes(fileBytes.Length);
+
+                Buffer.BlockCopy(sizeInBytesRepresentation, 0, buffer, bufferOffset, sizeInBytesRepresentation.Length);
+                bufferOffset += sizeInBytesRepresentation.Length;
+
+                Buffer.BlockCopy(fileBytes, 0, buffer, bufferOffset, fileBytes.Length);
+                bufferOffset += fileBytes.Length;
+            }
+
+            return buffer;
+        }
+
+        private static byte[] prepareBuffer(Dictionary<string, int> fileToSizeMap)
+        {
+            long totalBufferSize = 0;
+            foreach ((string _, int size) in fileToSizeMap)
+            {
+                totalBufferSize += size + 4; // 4 for the size represented in 4 bytes.
+            }
+
+            return new byte[totalBufferSize];
+        }
+    }
+}
